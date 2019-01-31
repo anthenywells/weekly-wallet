1291f1bdf90ff1cc60db447aae8715cef154862d
Stuart <bristol.stuart@gmail.com>
2019-01-31 11:45:51 -0500

    added modal functionality

--- script/script.js
+++ script/script.js
@@ -5,6 +5,16 @@ $(document).ready(() => {
   const expenceList = [];
   let counter = 0;
   
+  function openModal() { //function that opens the Expense Input Modal when the "Add Expense" button is clicked
+    $(".open").on("click", function(){
+        $(".popup, .popup-content").addClass("active"); //gives the DOM elemenets .popup and .popup-content a class of active
+        });
+        $(".close, .popup").on("click", function(){ // removes the class of active when the user "adds" a new expense
+            $(".popup, .popup-content").removeClass("active");
+        });
+    }
+openModal();
+
     $("#expButton").on("click", function(){
       if ($("#category").val() === "Entertainment") {
         expenceList.push({
