c7e4806cead72820ab9b86dd11f17aa882fc5ab9
Antheny Wells <anthenywells@Go-away.local>
2019-01-31 10:11:40 -0500

    implemented list / sorting jQuery

---
+++ script/script.js
@@ -0,0 +1,61 @@
+"use strict";
+
+$(document).ready(() => {
+
+  const expenceList = [];
+  let counter = 0;
+  
+    $("#expButton").on("click", function(){
+      if ($("#category").val() === "Entertainment") {
+        expenceList.push({
+          input: $(".input").val(),
+          id: counter++
+        });
+        $("#expenseList").append(`
+        <li class="entertainment">${$("#input").val()}
+        <button class="delete"><i class="fas fa-trash"></i></button>
+        </li>`
+        );
+      } else if ($("#category").val() === "Bills") {
+        expenceList.push({
+          input: $(".input").val(),
+          id: counter++
+        });
+        $("#expenseList").append(`
+        <li class="bills">${$("#input").val()}
+        <button class="delete"><i class="fas fa-trash"></i></button>
+        </li>`
+        );
+      } else if ($("#category").val() === "Food") {
+        expenceList.push({
+          input: $(".input").val(),
+          id: counter++
+        });
+        $("#expenseList").append(`
+        <li class="food">${$("#input").val()}
+        <button class="delete"><i class="fas fa-trash"></i></button>
+        </li>`
+        );
+      } else if ($("#category").val() === "Clothing") {
+        expenceList.push({
+          input: $(".input").val(),
+          id: counter++
+        });
+        $("#expenseList").append(`
+        <li class="clothing">${$("#input").val()}
+        <button class="delete"><i class="fas fa-trash"></i></button>
+        </li>`
+        );
+      } else if ($("#category").val() === "Misc") {
+        expenceList.push({
+          input: $(".input").val(),
+          id: counter++
+        });
+        $("#expenseList").append(`
+        <li class="misc">${$("#input").val()}
+        <button class="delete"><i class="fas fa-trash"></i></button>
+        </li>`
+        );
+      }
+    });
+  });
